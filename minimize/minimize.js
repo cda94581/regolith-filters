d166ca315bd4e2a9cb2bb6d63971532932bd0ae5
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-10-23 23:28:58 -0500

    Run BP

--- minimize/minimize.js
+++ minimize/minimize.js
@@ -8,3 +8,11 @@ glob('RP/**/*.json', (err, files) => {
 		fs.writeFile(f, min, 'utf-8', err => { if (err) throw err; });
 	});
 });
+
+glob('BP/**/*.json', (err, files) => {
+	files.forEach(f => {
+		const min = JSON.stringify(JSON.parse(fs.readFileSync(f, 'utf-8')));
+		console.log(`Minimizing ${f}...`);
+		fs.writeFile(f, min, 'utf-8', err => { if (err) throw err; });
+	});
+});

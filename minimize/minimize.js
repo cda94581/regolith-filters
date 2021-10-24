c182eb055dc90f754330b14d91fd0914ddfab88e
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-10-23 23:40:20 -0500

    Fix

--- minimize/minimize.js
+++ minimize/minimize.js
@@ -1,7 +1,7 @@
 const glob = require('glob');
 const fs = require('fs');
 
-glob('(B|R)P/**/*.json', (err, files) => {
+glob('@(B|R)P/**/*.json', (err, files) => {
 	files.forEach(f => {
 		const min = JSON.stringify(JSON.parse(fs.readFileSync(f, 'utf-8')));
 		console.log(`Minimizing ${f}...`);

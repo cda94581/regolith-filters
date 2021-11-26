5b59e9ca0b1deae12a50c644d4a5ba0568ac7629
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-11-26 13:40:13 -0600

    namespace version 0.0.2

--- namespace/README.md
+++ namespace/README.md
@@ -3,21 +3,20 @@ This filter automatically applies the namespace to all files. Useful for tempora
 
 The filter works by extracting the identifier from each file and splitting it into a namespace and an identifier. If no namespace is present, the namespace is then applied. If a namespace is already present, it will run through the settings to make sure the namespace is allowed to be replaced, and then replace it. By default, the "minecraft" and "minecon" namespaces will never be replaced.
 
-This only affects the namespace in the `description -> identifier`. Event namespaces and such are still a WIP.
-
 ## Usage
 This filter requires that you have [nodejs](https://nodejs.org/en/) installed.
 
 ```json
 {
-	"namespace": "test",
+	"namespace": "namespace",
 	"regolith": {
 		"profiles": {
 			"<Profile>": {
 				"filters": [{
 					"url": "github.com/cda94581/regolith-filters/namespace",
 					"settings": {
-						"ignored_namespaces": [ "tnt" ]
+						"type": "keys",
+						"ignoredNamespaces": [ "tnt" ]
 					}
 				}]
 			}
@@ -30,4 +29,34 @@ This filter requires that you have [nodejs](https://nodejs.org/en/) installed.
 
 Name | Default | Description
 ---- | ------- | -----------
-`ignored_namespaces` | `[]` | A list of namespaces to ignore, you don't want to change these namespaces for whatever reason.
+`type` | `keys` | The type of namespace replacement to use.
+`ignoredNamespaces` | `[]` | A list of namespaces to ignore, you don't want to change these namespaces for whatever reason.
+`oldNamespace` | `namespace` | The previous namespace, which will be replaced.
+
+#### type
+There are two possible values for the `type` setting. A detailed explanation is found below.
+
+##### keys
+**DUE TO ONGOING DEVELOPMENT, THIS TYPE HAS BEEN TEMPORARILY DISABLED.**
+
+The `keys` type runs through the key/value pairs of all the files and finds any of the keys or values which follow the format `namespace:identifier`, or should do so, and replaces the key/values as such.  
+As of right now, this only applies to the identifier in the description, however others are planned soon.  
+This is the recommended type, as it is safer to use. This type uses the `ignoredNamespaces` setting.
+
+##### find
+This is an agressive type which runs through the files and replaces everything that matches a pattern, specified by `oldNamespace`. This guarantees those with a namespace are replaced.  
+While using this type, be extremely cautious in your file to not use the string where you don't want it to be replaced. Doing so will replace string. This type uses the `oldNamespace` setting.
+
+## Changelogs
+### 0.0.2
+- Changed default new namespace from `test` to `newNamespace`
+- Changed ignored namespaces key setting `ignored_namespaces` to `ignoredNamespaces`
+- Changed default ignored namespaces from `[ 'test', 'minecraft', 'minecon' ]` to `[ 'minecraft', 'minecon' ]`
+- Added `type` setting
+- Moved code into the `keys` type
+- Temporarily disabled `keys` type
+- Implemented the `find` type
+- Added `oldNamespace` setting
+
+### 0.0.1
+- Released first version
--- namespace/namespace.js
+++ namespace/namespace.js
@@ -1,41 +1,87 @@
 const glob = require('glob');
-const fs = require('fs');
-let { ignored_namespaces } = JSON.parse(process.argv[2]);
-const { namespace } = JSON.parse(fs.readFileSync('../../config.json', 'utf-8'));
-if ( !namespace || namespace == undefined ) namespace = 'test';
-if ( !ignored_namespaces || ignored_namespaces == undefined ) ignored_namespaces = [];
+const fs = require('fs-extra');
+
+// Defaults
+let settings = process.argv[2];
+let type = 'keys';
+let ignoredNamespaces = [ 'minecraft', 'minecon' ];
+let oldNamespace = 'namespace'
+let namespace = 'newNamespace';
+// User
+try {
+	settings = JSON.parse(settings);
+	type = settings.type;
+	ignoredNamespaces = settings.ignoredNamespaces;
+	oldNamespace = settings.oldNamespace;
+	namespace = JSON.parse(fs.readFileSync('../../config.json', 'utf-8')).namespace;
+} catch {}
 
 glob('@(B|R)P/@(blocks|items|entities|entity|recipes|attachables|particles|fogs)/**/*.json', (err, files) => {
 	files.forEach(f => {
+		// Setup
 		let file = JSON.parse(fs.readFileSync(f, 'utf-8'));
-		const tlos = [
-			'minecraft:block',
-			'minecraft:item',
-			'minecraft:entity',
-			'minecraft:client_entity',
-			'minecraft:recipe_furnace',
-			'minecraft:recipe_shaped',
-			'minecraft:recipe_shapeless',
-			'minecraft:recipe_brewing_mix',
-			'minecraft:recipe_brewing_container',
-			'minecraft:attachable',
-			'particle_effect',
-			'minecraft:fog_settings'
-		];
-		let identifier;
-		tlos.forEach(tlo => { if (file[tlo]) identifier = file[tlo].description.identifier.split(':', 2); });
-		if (identifier == undefined) return;
-		if (identifier.length == 1) {
-			console.log(`Setting the namespace for ${f}`);
-			identifier[1] = identifier[0];
-			identifier[0] = namespace;
-		} else {
-			if (ignored_namespaces.includes(identifier[0]) || identifier[0] == 'minecraft' || identifier[0] == 'minecon') return;
-			console.log(`Replacing the namespace for ${f}`)
-			identifier[0] = namespace;
-		}
-		tlos.forEach(tlo => { if (file[tlo]) file[tlo].description.identifier = identifier.join(':'); });
+		eval(`${type}()`);
 
-		fs.writeFile(f, JSON.stringify(file, null, '\t'), err => { if (err) throw err; });
+		function keys() {
+			return console.log('The type "keys" is temporarily disabled due to active development.');
+		// 	const tlos = [
+		// 		'minecraft:block',
+		// 		'minecraft:item',
+		// 		'minecraft:entity',
+		// 		'minecraft:client_entity',
+		// 		'minecraft:recipe_furnace',
+		// 		'minecraft:recipe_shaped',
+		// 		'minecraft:recipe_shapeless',
+		// 		'minecraft:recipe_brewing_mix',
+		// 		'minecraft:recipe_brewing_container',
+		// 		'minecraft:attachable',
+		// 		'particle_effect',
+		// 		'minecraft:fog_settings'
+		// 	];
+		// 	let type;
+		// 	tlos.forEach(tlo => { if (file[tlo]) type = tlo; });
+	
+		// 	// Generate Data
+		// 	function keyify (obj, prefix = '') {
+		// 		let keys = Object.keys(obj);
+		// 		for ( let i = 0; i < keys.length; i++ ) {
+		// 			const preKey = prefix + keys[i];
+		// 			if ( !Array.isArray(obj[keys[i]]) && obj[keys[i]] != null && typeof obj[keys[i]] == 'object' ) keys.push(...keyify(obj[keys[i]], preKey + '.' ));
+		// 			else keys[i] = preKey;
+		// 		};
+		// 		return keys;
+		// 	}
+		// 	const nsPaths = keyify(file[type]).filter(path => path.endsWith('.event'));
+		// 	console.log(nsPaths);
+		// 	// Somehow I need to find all the "event" keys, as well as the events and component groups. Hmm
+	
+		// 	// Replace namespaces
+		// 	nsPaths.forEach(path => {
+		// 		const ns = eval(`file[type][${path}].split(':', 2)`);
+		// 		if (!ns) return;
+		// 		if (ns.length == 1) {
+		// 			console.log(`Setting the namespace for ${f} -> ${path}`);
+		// 			ns = [ namespace, ns[0] ];
+		// 		} else {
+		// 			if (ignoredNamespaces.includes(ns[0])) return;
+		// 			console.log(`Replacing the namespace for ${f} -> ${path}`);
+		// 			ns[0] = namespace;
+		// 		}
+		// 		eval(`file[type][${path}]`) = ns.join(':');
+		// 	});
+		}
+
+		function find() {
+			file = JSON.stringify(file, null, '\t');
+			file = file.replace(new RegExp(oldNamespace, 'g'), namespace);
+			file = JSON.parse(file);
+			if (f.match(new RegExp(oldNamespace))) {
+				fs.unlinkSync(f);
+				f = f.replace(new RegExp(oldNamespace, 'g'), namespace);
+			}
+		}
+
+		// Write out file
+		fs.outputFileSync(f, JSON.stringify(file, null, '\t'), err => { if (err) throw err; });
 	});
 });

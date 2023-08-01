1df3d12682fdd28bc7803c6f9a349f647ec737bf
cda94581 <76138236+cda94581@users.noreply.github.com>
2023-07-31 20:44:15 -0500

    Export 0.0.4

--- export/README.md
+++ export/README.md
@@ -1,5 +1,5 @@
 # export
-Version **0.0.3**
+Version **0.0.4**
 
 This filter exports the behavior pack and resource pack into a ".mcaddon" file, ready for distribution.
 
@@ -16,7 +16,19 @@ Install this filter by running (`regolith install github.com/cda94581/regolith-f
 		{
 			"filter": "export",
 			"settings": {
-				"exclude": [ "RP" ]
+				"output": "myAddon{{var1}}.mcaddon",
+				"variables": {
+					"var1": "Hi"
+				},
+				"exclude": [ "RP" ],
+				"include": [
+					{
+						"source": "README.md",
+						"fromRoot": false,
+						"path": "README.md",
+						"format": "md"
+					}
+				]
 			}
 		}
 	]
@@ -27,12 +39,60 @@ Install this filter by running (`regolith install github.com/cda94581/regolith-f
 
 Name | Default | Description
 ---- | ------- | -----------
-`exclude` | `[]` | Exclude BPs or RPs from being exported.
+`output` | `{{name}}.mcaddon` | Output file path
+`variables` | `{}` | Defines some variables that can be used in the `output` setting
+`exclude` | `[]` | Exclude BPs or RPs from being exported
+`include` | `[]` | Include additional files/folders that may assist with packaging
+
+#### output
+Will be exported to the root of the project. Can use custom [variables](#variables) in the path
+
+#### variables
+Object in key/value formats for custom variables in the [`output`](#output) field. Referenced with the `{{variable}}` syntax
 
 #### exclude
 Allowed values: `BP`, `RP`
 
+#### include
+Array of objects which include additional properties
+
+##### source
+REQUIRED
+
+Source of the file/folder in relation to the `packs/data` folder by default (see [fromRoot](#fromroot))
+
+##### fromRoot
+OPTIONAL
+
+Used with [source](#source), if set to `true`, the source will be in relation to the root of the project
+
+If not included, this behaves as through it is set to false
+
+##### path
+OPTIONAL
+
+Output path in relation to the root of the zip
+
+If not included, this will be the same as the source, except it will be located directly in the root of the zip
+
+##### format
+OPTIONAL
+
+Allowed values: `md`, `txt`, `none`
+
+All this does is mess with the § text formatting to make it easier to use one file for multiple purposes
+- `md` also works for html files, essentially replaces all of them with proper `<span>` colored/formatted text
+- `txt` completely strips all of the formatting for a "clean" text experience
+- `none` behaves as if the option does not exist
+
+If not included, this will not change the text files at all
+
 ## Changelog
+### 0.0.4
+- Added new settings to assist with exporting and packaging add-ons: [output](#output), [variables](#variables), [include](#include)
+- Refactored how exporting works with the use of the `ROOT_DIR` enviornment variable
+- Fixed a bug in which removing the `exclude` setting could cause issues
+
 ### 0.0.3
 - Updated filter to reflect proper description syntax
 
--- export/export.js
+++ export/export.js
@@ -1,18 +1,86 @@
 const archiver = require('archiver');
+const glob = require('glob');
 const fs = require('fs');
 const { name } = JSON.parse(fs.readFileSync('../../config.json', 'utf-8'));
+const { ROOT_DIR } = process.env;
+const formatCodes = require('./formatCodes.json');
 
 let settings = process.argv[2];
+let output = `${name}.mcaddon`;
+let variables = {};
 let exclude = [];
+let include = [];
 try {
 	settings = JSON.parse(settings);
-	exclude = settings.exclude;
+	output = settings.output || output;
+	variables = settings.variables || variables;
+	exclude = settings.exclude || exclude;
+	include = settings.include || include;
 } catch {}
 
-const output = fs.createWriteStream(`../../${name}.mcaddon`, 'utf-8');
+for ([key, value] of Object.entries(variables)) output = output.replaceAll(`{{${key}}}`, value);
+output = `${ROOT_DIR}/${output}`;
+
+const stream = fs.createWriteStream(output, 'utf-8');
 const archive = archiver('zip', { zlib: { level: 9 }});
 
 ['bp', 'rp'].filter(x => !exclude.some(y => y.toLowerCase() == x)).forEach(x => { archive.directory(x.toUpperCase(), x.toLowerCase()); });
-archive.on('error', err => console.error(err));
-archive.pipe(output);
+include.forEach(f => {
+	let { source } = f;
+	let sourcePath;
+	if (f.fromRoot) sourcePath = `${ROOT_DIR}/${source}`;
+	else sourcePath = `data/${source}`;
+	const stats = fs.statSync(sourcePath, { throwIfNoEntry: false });
+	if (!stats) return console.error(`File does not exist (skipping file): ${source}`);
+	const name = (f.path || source).split('/');
+
+	if (stats.isDirectory()) {
+		const files = glob.sync(`${sourcePath}/**/*`);
+		if (f.format && f.format != 'none') {
+			const fixF = new Map();
+			files.forEach(file => {
+				const s = fs.statSync(file);
+				if (s.isDirectory()) return;
+				const text = fs.readFileSync(file, 'utf-8');
+				const fix = f.format == 'txt' ? formatText(text) : formatMd(text);
+				fixF.set(file, fix);
+			});
+			fixF.forEach((fix, file) => {
+				const name2 = file.slice(sourcePath.length);
+				archive.append(fix, { name: name + name2 });
+			});
+		}
+		else archive.directory(sourcePath, name[name.length - 1]);
+	}
+	else {
+		const text = fs.readFileSync(sourcePath, 'utf-8');
+		const fix = f.format == 'txt' ? formatText(text) : formatMd(text);
+		archive.append(fix, { name: name[name.length - 1] });
+	}
+});
+
+archive.on('error', console.error);
+archive.pipe(stream);
 archive.finalize();
+
+function formatText(text) {
+	text = text.replaceAll(new RegExp('§.', 'g'), '');
+	return text;
+}
+
+function formatMd(text) {
+	const valid = Object.keys(formatCodes);
+	const matches = [...text.matchAll(`§[${valid.join('')}r]`)];
+	let resetSpan = '';
+	matches.forEach(match => {
+		if (match[0] === '§r') {
+			text = text.replace(match[0], resetSpan);
+			resetSpan = '';
+			return;
+		}
+		text = text.replace(match[0], `<span style="${formatCodes[match[0][1]]}">`);
+		resetSpan += '</span>';
+	});
+	[...text.matchAll(/.\n[^\n]/g)].forEach(match => text = text.replace(match[0], match[0][0] + '  \n' + match[0][2]));
+	return text;
+}
